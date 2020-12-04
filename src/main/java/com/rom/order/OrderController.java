7c7d0273f42e6c994adde50711a109fbdc8c2ec6
prasadbayi <74462843+prasadbayi@users.noreply.github.com>
2020-12-05 01:23:26 +0530

    @CrossOrigin(origins = "*")

--- src/main/java/com/rom/order/OrderController.java
+++ src/main/java/com/rom/order/OrderController.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("rom")
+@CrossOrigin(origins = "*")
 public class OrderController {
 	private Logger log = LogManager.getLogger(OrderController.class);
 
