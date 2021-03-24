296d75d80e159d0f373813ca030856aa4bfd7a20
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2021-03-24 08:30:16 +0530

    Tried integrating OAuth as per below, and commented for now.
    List of files added/modified are:
    1. applicaiton.yml
    2. pom.xml
    3. SecurityConfig.java
    
    https://www.baeldung.com/rest-api-spring-oauth2-angular

---
+++ src/main/java/com/rom/oauth/SecurityConfig.java
@@ -0,0 +1,28 @@
+/*package com.rom.oauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+            .and()
+              .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/user/info", "/rom/**")
+                  .hasAuthority("SCOPE_read")
+                .antMatchers(HttpMethod.POST, "/rom")
+                  .hasAuthority("SCOPE_write")
+                .anyRequest()
+                  .authenticated()
+            .and()
+              .oauth2ResourceServer()
+                .jwt();
+    }
+}
+
+*/
